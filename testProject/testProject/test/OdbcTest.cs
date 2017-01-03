1f6d48627432fc23a4ded5119c57c0ebff4f85f1
nvd11@163.com <nvd11@163.com>
2017-01-03 23:40:45 +0800

    test dataset

--- testProject/testProject/test/OdbcTest.cs
+++ testProject/testProject/test/OdbcTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.Odbc;
 using System.Linq;
 using System.Text;
@@ -17,8 +18,27 @@ namespace com.testProject.test
 
             con.Open();
             OdbcCommand com = new OdbcCommand(sql, con);
-            int i = Convert.ToInt32(com.ExecuteScalar());
-            Console.WriteLine(i);
+            int j = Convert.ToInt32(com.ExecuteScalar());
+            Console.WriteLine(j);
+
+
+            OdbcCommand sc = new OdbcCommand("select clntcode, clntname from tClient", con);
+            //OdbcCommand sc = new OdbcCommand("SELECT title, price FROM titles WHERE title LIKE 'Hi%'", cn);
+            OdbcDataAdapter da = new OdbcDataAdapter(sc);
+            DataSet ds = new DataSet("TitlesDS");
+            da.Fill(ds);
+
+            DataTable dt = ds.Tables[0];
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                //遍历列
+                for (int i = 0; i < dt.Columns.Count; i++)
+                {
+                    Console.WriteLine(dr[i].ToString());
+                }
+            }
+            Console.WriteLine("done");
             return true;
         }
 
